302dec431b790005f649d3520bd01cd5f9a0dfa8
wntjdjus <wntjdjus@naver.com>
2021-11-23 00:36:09 +0900

    오타수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ function switch_proxy() {
 
     echo "> 전환할 포트: $IDLE_PORT"
     echo "> 포트 전환"
-    echo ">set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
     echo "> 엔진엑스 reload"
     sudo service nginx reload
 }
