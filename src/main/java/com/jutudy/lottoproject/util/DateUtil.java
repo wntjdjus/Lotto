fd8b0942453c340970d7f74c90ce10d74eaf3873
wntjdjus <wntjdjus@naver.com>
2021-12-11 02:28:10 +0900

    t

--- src/main/java/com/jutudy/lottoproject/util/DateUtil.java
+++ src/main/java/com/jutudy/lottoproject/util/DateUtil.java
@@ -11,10 +11,12 @@ public class DateUtil {
         calendar.setTime(date);
 
         int dayNum = calendar.get(Calendar.DAY_OF_WEEK);
+        int before = -1;
         // 토요일은 7, 토요일이 아니면 가장 직전 토요일을 찾는다.
-        if (dayNum != 7) {
-            calendar.add(Calendar.DATE, (-1) * dayNum);
+        if (dayNum == 7) {
+            before = -7;
         }
+        calendar.add(Calendar.DATE, (-1) * dayNum);
 
         return calendar.getTime();
     }
